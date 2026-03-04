90ed067bb971133367c70625791fb820f38e43d5
Guest1511-WS2 <brandon_rogers1511@daytonastate.edu>
2026-03-03 19:35:04 -0500

    Update mongodb.ts

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,5 +1,7 @@
 import { MongoClient, MongoClientOptions } from 'mongodb';
 
+MONGODB_URI = "mongodb+srv://Vercel-Admin-cookbook-database-2:SCX21F1Ho0aGurw0@cookbook-database-2.qi2avgg.mongodb.net/?retryWrites=true&w=majority";
+
 const uri = process.env.MONGODB_URI;
 const options: MongoClientOptions = {
   appName: "devrel.nextjs.starter",
