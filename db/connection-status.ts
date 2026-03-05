2984b89548ddb366a513bf601667f22d674c0137
Guest1511-WS2 <brandon_rogers1511@daytonastate.edu>
2026-03-05 13:48:34 -0500

    Update connection-status.ts
    testing result output

--- db/connection-status.ts
+++ db/connection-status.ts
@@ -22,7 +22,7 @@ export async function dbConnectionStatus() {
       })
       .limit(10)
       .toArray();
-    console.log("MongoDB connection successful" + JSON(result));
+    console.log("MongoDB connection successful" + result);
     return "Database connected";
   } catch (error) {
     console.error("Error connecting to the database:", error);
