9c0d41ad96a1aeafcf0d99f524b3ebefa739a703
Guest1511-WS2 <brandon_rogers1511@daytonastate.edu>
2026-03-06 13:50:50 -0500

    Update connection-status.ts
    attempting to make databases global

--- db/connection-status.ts
+++ db/connection-status.ts
@@ -10,10 +10,20 @@ export async function dbConnectionStatus() {
   try {
     const client = await clientPromise;
     const db = client.db("cooking_inventory");
-    const users = db.collection("cooking_inventory_users");
-    const ingredients = db.collection("ingredientInventory");
-    const recipes = db.collection("recipesInventory");
+    declare users = db.collection("cooking_inventory_users");
+    declare ingredients = db.collection("ingredientInventory");
+    declare recipes = db.collection("recipesInventory");
     console.log("MongoDB connection successful");
+    const result = await ingredients
+      .find({})
+      .project({
+        name: 0,
+        amount: 0,
+        unit: 0,
+      })
+      .limit(10)
+      .toArray();
+    console.log(ingredients);
     return "Database connected";
   } catch (error) {
     console.error("Error connecting to the database:", error);
