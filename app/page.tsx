bfeb9a5018400fe13b599b1234c9ed7eb21cfde5
Guest1511-WS2 <brandon_rogers1511@daytonastate.edu>
2026-03-06 13:53:31 -0500

    Update page.tsx
    resetting to normal conditions part 2

--- app/page.tsx
+++ app/page.tsx
@@ -37,7 +37,6 @@ const DATA = {
 
 export default async function Home() {
   const result = await dbConnectionStatus();
-  console.log(dbConnectionStatus.ingredients);
   return (
     <div className="flex min-h-screen flex-col">
       <div className="mx-auto flex w-full max-w-md flex-1 flex-col px-5 md:max-w-lg md:px-0 lg:max-w-xl">
